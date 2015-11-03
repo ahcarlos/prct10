20556a236ce84da70e9ab8c288e445ce5910c8bb
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2015-11-03 15:37:24 +0000

    Terminada la clase

--- lib/Bibliografia/Bibliografia.rb
+++ lib/Bibliografia/Bibliografia.rb
@@ -14,4 +14,72 @@ module Bibliografia
 	    @isbn = i
 	    @referencia = r
 	  end
+		def GetAutor()
+			@autor
+		end
+
+		def SetAutor(valor)
+			@autor = valor
+		end
+		
+	  def SetTitulo(valor)
+	    @titulo = valor
+	  end
+
+	  def GetTitulo()
+	    @titulo
+	  end
+
+	  def SetEditorial(valor)
+	    @editorial = valor
+	  end
+
+	  def GetEditorial()
+	    @editorial
+	  end
+
+	  def SetEditorial(valor)
+	    @editorial = valor
+	  end
+
+	  def GetNum_edicion()
+	    @num_edicion
+	  end
+
+	  def SetNum_edicion(valor)
+	    @num_edicion = valor
+	  end
+
+	  def GetSerie()
+	    @serie
+	  end
+
+	  def SetSerie(valor)
+	    @serie = valor
+	  end
+
+	  def GetFecha_publicacion()
+	    @fecha_publicacion
+	  end
+
+	  def SetFecha_publicacion(valor)
+	    @fecha_publicacion = valor
+	  end
+
+	  def GetIsbn()
+	    @isbn
+	  end
+
+	  def SetIsbn(valor)
+	    @isbn = isbn
+	  end
+
+	  def GetReferencia()
+	    @referencia
+	  end
+
+	  def SetReferencia(valor)
+	    @referencia = valor
+	  end
+	end
 end
