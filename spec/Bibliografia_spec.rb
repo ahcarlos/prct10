34c05627eff16e3ef1af60d7f03640540891f058
alu0100816167 <alu0100816167@ull.edu.es>
2015-11-03 16:11:43 +0000

    Primera prueba realizada titulo del libro1

--- spec/Bibliografia_spec.rb
+++ spec/Bibliografia_spec.rb
@@ -9,4 +9,16 @@ describe Bibliografia do
     
     l1= Bibliografia::Bibliografia.new("De viaje por Europa del Este", autores[0], editoriales[0], 2292, 23232, 2015, 23231, 243424)
     l2= Bibliografia::Bibliografia.new("Pideme lo que quieras y te lo dare", autores[1], editoriales[1], 4882, 2910019226453, 2015, 222, 222)
+    
+    context "# Almacenamiento de las variables del libro 1" do
+        it "Se almacena el titulo" do
+            expect(l1.GetTitulo).to eq("De viaje por Europa del Este")
+        end
+        
+
+            
+    end    
+    
+    
 end
+
