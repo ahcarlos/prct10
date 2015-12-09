9066a7591ee19eb708eb4e254c0bf86f60d534a2
alu0100816167 <alu0100816167@ull.edu.es>
2015-12-09 12:13:42 +0000

    añadiendo coveralls

--- spec/Bibliografia_spec.rb
+++ spec/Bibliografia_spec.rb
@@ -2,95 +2,96 @@ require 'spec_helper'
 require 'Bibliografia'
 require 'Bibliografia/list'
 
-describe List do
-     
 
-    context "#Pruebas include enumerable" do
-   
-       before :all do
-            @libro1= Bibliografia::Libro.new("Los pilares de la Tierra","Ken Folletdd", 2013, 1449325866, "edition", 6)
-            @libro2= Bibliografia::Libro.new("Dama del Nilo","Pauline Gedge", 2010, 1449325555866, "Mi editorial", 3) 
-            @libro3= Bibliografia::Libro.new("Serie Revolucion","Simon Scarrow", 2000, 14866, "edition1", 1)
-            @libro4= Bibliografia::Libro.new("Ramos y su perrito del patio","Simon Scarrow", 2000, 1499899866, "edition1", 166)
-            
-            @list1 = List::List.new()
-            @list2 = List::List.new()
-            
-            @list1.insert_single_beg(@libro1)
-            @list1.insert_single_beg(@libro2)
-            @list1.insert_single_beg(@libro3)
-            @list1.insert_single_beg(@libro4)
-        end
-        
-        
-         it "#Comprobrando max. El   maximo es Serie Revolucion porque es la letra mas cercana a la Z" do
-         expect(@list1.max).to eq(@libro3)
-        end
-        
-        it "# Comprobrando min. El minimo es Dama del Nilo porque es la letra mas cercana a la A" do
-         expect(@list1.min).to eq(@libro2)
-        end
-        
-        it "# Comprobando sort" do
-            expect(@list1.sort).to eq([@libro2, @libro1, @libro4, @libro3])
-        end
-        
-        it "# Comprobrando el metodo 'all?' con un bloque vacio" do
-        #all? -> The method returns true if the block never returns false or nil
-          
-          expect(@list2.all?).to eq(false)
-        end 
-        
-        it "# Comprobrando el metodo 'any?' " do
-        #any -> The method returns true if the block ever returns a value other than false or nil
-          expect(@list1.any?).to eq(true)
-          expect(@list2.any?).to eq(false)
-        end 
-        
-        it "# Comprobando el metodo take" do
-            #Returns an array containing the first n (2) elements
-            expect(@list1.take(2)).to eq([@libro4, @libro3])
-        end
-        
-        it "# Comprobando el metodo find" do
-            expect(@list1.find{|i| i== @libro3}).to eq(@libro3)
-        end
-
-        
-    end
-        
 describe Bibliografia do
             
-        context "#Pruebas include comparable" do
+        context "#Pruebas con libros" do
             
             before :all do
-                @libro1= Bibliografia::Libro.new("Git Pocket Guide O'Reilly Media","David Chelimsky", 2013, 1449325866, "edition", 1)
-                @libro2= Bibliografia::Libro.new("Viaje Alrededor del Mundo","Samuel Ramos", 2010, 1449325555866, "Mi editorial", 3)
-                @libro3= Bibliografia::Libro.new("Una historia de miedo","Alberto Barrera", 2015, 1449325555866, "Mi editorial", 3)
-                @libro4= Bibliografia::Libro.new("Viaje Alrededor del Mundo","Samuel Ramos", 2010, 1449325555866, "Mi editorial", 3)
-                @libro5= Bibliografia::Libro.new("Dama del Nilo","Pauline Gedge", 2010, 1449325555866, "Mi editorial", 3) 
-                @libro6= Bibliografia::Libro.new("Serie Revolucion","Simon Scarrow", 2000, 14866, "edition1", 1)
+               
+                @libro1 = Bibliografia::Libro.new("Follet, K.", 2015, "Los pilares de la tierra","5º edición", "volumen 1", "Lugar publicacion", "Salamandra")
+                @libro2 = Bibliografia::Libro.new("Autor, B. A.", 2015, "AAAA","5º edición", "volumen 1", "Lugar 2", "Anaya" )
+                @libro3 = Bibliografia::Libro.new("Autor, B. A.", 2019, "AAAA","5º edición", "volumen 1", "Lugar 3", "Libros")
+                @libro4 = Bibliografia::Libro.new("Autor, B. A.", 2019, "Titulo diferente a libro 3","5º edición", "volumen 1", "Lugar 4", "Mieditorial")
+                
+                @lista1= List::List.new()
+                @lista2 = List::List.new()
+                @lista3 = List::List.new()
+                
+                @lista1.insert_single_beg(@libro1)
+                @lista1.insert_single_beg(@libro2)
+                
+                @lista2.insert_single_beg(@libro2)
+                @lista2.insert_single_beg(@libro3)
+                
+                @lista3.insert_single_beg(@libro3)
+                @lista3.insert_single_beg(@libro4)
+                
+                
+              
+                
             end
             
-            it "# El libro1 (G) es menor (<) estricto que el libro 2 (V) " do
-                 expect(@libro1 < @libro2).to eq(true)
+            
+             it "Los autores son distintos" do
+                expect(@lista1.sort).to eq([@libro2,@libro1])
+             end
+         
+            it "Los autores son iguales,ordenamos por fechas" do
+                expect(@lista2.sort).to eq([@libro2,@libro3])
             end
             
-            it "# El libro1 (G) es menor o igual (<=) al  libro 2 (V) " do
-                 expect(@libro1 <= @libro2).to eq(true)
+            it "Los autores son iguales, las fechas son iguales, ordenando por título" do
+                expect(@lista3.sort).to eq([@libro3,@libro4])
             end
             
-            it"# El libro2 es igual (==) al libro4" do
-                expect(@libro2 == @libro4).to eq(true)
+            it "Mostrando en formato APA" do
+                expect(@libro1.to_s()).to eq("Follet, K.(2015). Los pilares de la tierra(5º edición) (volumen 1). Lugar publicacion: Salamandra.")
             end
             
-            it "# El libro6 (S) es mayor estricto (>) que el libro5 (D)" do
-                expect(@libro6 > @libro5).to eq(true)
+        end
+        
+        context "#Pruebas con todas las referencias" do
+            
+            before :all do
+                @libro = Bibliografia::Libro.new("Autor, A. A.", 2020, "AAAA","5º edición", "vol 1", "Marte", "Salamandra")
+                @articulo = Bibliografia::Articulo.new("Autor, B. A.", 2020, "BBBB", "A. Editor","AAAA",50,"5ª edición","vol 1", "Marte", "Salamandra")
+                @doc = Bibliografia::Edoc.new("Autor, D. B.", 2014,"DDDD","1ª edición","tipo de medio","Mercurio","Editor","url",2015)
+    
+                @lista1 = List::List.new()
+                
+                @lista1.insert_single_beg(@libro)
+                @lista1.insert_single_beg(@articulo)
+                @lista1.insert_single_beg(@doc)
+                
+                
+                @libro2 = Bibliografia::Libro.new("Follet, K. & Dueñas, M. ", 1992, "AAAA","edición", "vol 7", "Editado", "Editorial")
+                @articulo2 = Bibliografia::Articulo.new("Verne, J. & Lindo, E.", 2070, "BBBB", "Un Editor","AAAA",50,"5ª edición","vol 1", "Lugar publicacion", "perenquen")
+                @electronico2 = Bibliografia::Edoc.new("Lindo, E.", 1990,"FFFF","tercera edicion","medio","Mercurio","Editor","source",2015)
+                @electronico3 = Bibliografia::Edoc.new("Dueñas, M.", 1990,"FFFF","tercera edicion","medio","Mercurio","Editor","source",2015)
+                
+                
+                @lista2 = List::List.new()
+                @lista2.insert_single_beg(@electronico2)
+                @lista2.insert_single_beg(@articulo2)
+                @lista2.insert_single_beg(@libro2)
+                @lista2.insert_single_beg(@electronico3)
+                @lista2.sort
+              
+                
             end
             
-            it "# El libro6 (S) es mayor o igual (>=) que el libro5 (D)" do
-                expect(@libro6 >= @libro5).to eq(true)
+            it "Se ordena alfabeticamente por apellidos" do
+                expect(@lista1.sort).to eq([@libro,@articulo, @doc])
             end
+            
+            
+=begin  
+             it "Se ordena por autores individuales en caso de que haya más de uno " do
+                expect(@lista2.inicio).to eq(@electronico2)
+                expect(@lista2.fin).to eq(@electronico3)
+            end
+=end
             
         end
 end
@@ -98,7 +99,6 @@ end
 
 
 
-end
 
 
 
