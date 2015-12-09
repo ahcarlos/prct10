f977cafd0f82780d1f40106865f744c75cac37b0
alu0100816167 <alu0100816167@ull.edu.es>
2015-12-09 12:04:57 +0000

    Probar coveralls

--- lib/Bibliografia/Bibliografia.rb
+++ lib/Bibliografia/Bibliografia.rb
@@ -4,58 +4,90 @@ module Bibliografia
 	
 		include Comparable
 	
-		  def initialize(titulo, autor, fecha_publicacion) 
-		    @titulo= titulo
+		  def initialize(autor, fecha_publicacion, titulo) 
 		    @autor= autor
 		    @fecha_publicacion= fecha_publicacion
+		    @titulo= titulo
 		  end
 	
 		
 		def <=>(other)
 			#return nil unless other.kind_of? Bibliografia::Bibliografia
-			@titulo <=> other.titulo
+				
+			#return nil unless other.kind_of? Bibliografia::Bibliografia
+			if((@autor == other.autor)== 0)
+					if((@fecha_publicacion <=> other.fecha_publicacion)==0)
+						@titulo <=> other.titulo
+					else
+						@fecha_publicacion <=> other.fecha_publicacion
+					end
+			else
+				@autor <=> other.autor
+				
+			end
 		end
 		
-		def ==(other)
-			#return nil unless other.kind_of? Bibliografia::Bibliografia
-			@titulo == other.titulo
-		end
+		
+		
+		def to_s()
+	        			
+	    end
+		
 	end
+	
 	
 	
 	
 	class Libro < Bibliografia
-		attr_accessor :isbn, :editorial, :num_edicion
-		def initialize(titulo, autor, fecha_publicacion, isbn, editorial, num_edicion)
-			@titulo= titulo
-		    @autor= autor
-		    @fecha_publicacion= fecha_publicacion
-			@isbn= isbn
-			@editorial= editorial
-			@num_edicion= num_edicion
+		attr_accessor  :edicion, :volumen, :lpublicacion, :editor
+		def initialize(autor, fecha_publicacion, titulo, edicion, volumen, lug_pub, editor)
+		
+               @autor = autor
+               @fecha_publicacion = fecha_publicacion
+               @titulo = titulo
+               @edicion = edicion
+               @volumen = volumen 
+               @lpublicacion = lug_pub
+               @editor= editor
+               
+		end
+		
+		def to_s()
+			@s = "#{@autor}"+"("+"#{@fecha_publicacion}"+"). "+"#{@titulo}"+"("+"#{@edicion}"+") ("+"#{@volumen}"+"). "+"#{@lpublicacion}"+": "+"#{@editor}"+"."
 		end
 	end
 	
-	class Revista < Bibliografia
-		attr_accessor :issn,  :nombre_revista
-		def initialize(titulo, autor, fecha_publicacion, issn, nombre_revista)
-			@titulo= titulo
-		    @autor= autor
-		    @fecha_publicacion= fecha_publicacion
-			@issn= issn
-			@nombre_revista= nombre_revista
+	class Articulo < Bibliografia
+		attr_accessor  :eds, :titulo2, :pags, :edicion, :volumen, :lpublicacion, :editor
+		def initialize(autor, fecha_publicacion, titulo, eds, titulo2, pags, edicion, volumen, lug_pub, editor)
+			
+			@autor = autor
+            @fecha_publicacion = fecha_publicacion
+            @titulo = titulo
+            @eds = eds
+            @titulo2 = titulo2
+            @pags = pags
+            @edicion = edicion
+            @volumen = volumen
+            @lpublicacion = lug_pub
+            @editor = editor
+            
 		end
 		
 	end
 	
-	class Ebook < Bibliografia
-		attr_accessor :url, :formato
-		def initialize(titulo, autor, fecha_publicacion, url, formato)
-			@titulo= titulo
-		    @autor= autor
-		    @fecha_publicacion= fecha_publicacion
-			@url= url
-			@formato= formato
+	class Edoc < Bibliografia
+		attr_accessor :edicion, :tipo_medio, :lug_pub, :editor, :source, :fechacceso
+		def initialize( autor, fecha_publicacion, titulo, edicion, tipo_medio, lug_pub, editor, source, fechacceso)
+			@autor = autor
+            @fecha_publicacion = fecha_publicacion
+            @titulo = titulo
+            @edicion = edicion	
+            @tipo_medio = tipo_medio
+            @lpublicacion = lug_pub
+            @editor = editor
+            @source = source
+            @fechacceso = fechacceso
 		end
 	end
 end
